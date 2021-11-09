8957e3deb4495918baf1fe9be46841b9f9beb192
Yash Chandra <68777570+yash-chandra-23@users.noreply.github.com>
2021-11-09 16:08:39 +0530

    Program -1

---
+++ Program-1.c
@@ -0,0 +1,12 @@
+/* Week-1
+ Demonstration of FORK() System Call*/
+/* Single FORK Implementation */
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+fork();
+printf("Yash Chandra \n");
+return 0;
+
+}
